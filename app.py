aaa27d9ba5a39419b9879827eeac297df8d33310
RUHAN7777 <155990695+RUHAN7777@users.noreply.github.com>
2025-05-24 13:28:26 +0530

    Update app.py, better

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def get_db_connection():
         host = os.environ['MYSQL_HOST']
         user = os.environ['MYSQL_USER']
         password = os.environ['MYSQL_PASSWORD']
-        database = os.environ['MYSQL_DATABASE']
+        db = os.environ['MYSQL_DATABASE']
         port=int(os.environ.get('DB_PORT', 3306)),
         cursorclass=pymysql.cursors.DictCursor,
         connect_timeout=10
